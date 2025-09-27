7043bee30056c826e721fa9c1477b6b4d8553273
freit4sdev <chelmizinho@gmail.com>
2025-09-27 20:08:47 -0300

    feat: add gitignore

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/freitas/Desktop/projects/etec/barbearia-imperio/app/layout\":[\"static/media/eaead17c7dbfcd5d-s.p.woff2\",\"static/media/028c0d39d2e8f589-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # barbearia-imperio
+
+- git clone https://github.com/victor1326/barbearia-imperio.git
+- npm install
